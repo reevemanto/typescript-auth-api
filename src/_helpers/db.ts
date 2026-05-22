ecfffa9afbb9648e438302e87ab1c20087ebd9b6
reevemanto <christianmanto2004@gmail.com>
2026-05-22 22:08:13 +0800

    debug error 500 refresh token/ align configuration to new database(5)

--- src/_helpers/db.ts
+++ src/_helpers/db.ts
@@ -37,7 +37,7 @@ async function initialize() {
     db.Account = initAccount(sequelize);
     db.RefreshToken = initRefreshToken(sequelize);
 
-    await sequelize.sync({ alter: false });
+   // await sequelize.sync({ alter: false });
 }
 
 export { initialize };
